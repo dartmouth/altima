1bd8f991dad30229fcfd3124a4810508a8ea4e5e
Simon Stone <simon.stone@dartmouth.edu>
2023-06-29 13:07:31 -0400

    adds install command to install a module, optionally specifying the version number

--- cmd/install.go
+++ cmd/install.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"altima/pkg/repo"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -13,15 +14,47 @@ import (
 // installCmd represents the install command
 var installCmd = &cobra.Command{
 	Use:   "install",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Installs modules from the module index",
+	Long:  `Installs one or more modules from the module index. You can optionally supply a version number`,
+	Args:  cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("install called")
+
+		modules := getModules(args)
+
+		for _, module := range modules {
+			if module.Version != "" {
+				fmt.Printf("Searching URL for module %q in version %q...\n", module.Name, module.Version)
+			} else {
+				fmt.Printf("Searching URL for module %q...\n", module.Name)
+			}
+
+			url, err := repo.Search(module.Name, module.Version, settings.RepositoryCacheDir)
+
+			if err != nil {
+				msg := fmt.Sprintf("Failed to find module %q", module.Name)
+				if module.Version != "" {
+					msg += fmt.Sprintf(" in version %q", module.Version)
+				}
+				msg += " in index!"
+				fmt.Println(fmt.Errorf(msg))
+				continue
+			}
+
+			fmt.Printf("Found URL %q...\n", url)
+			fmt.Printf("Installing module %q...\n", module.Name)
+			err = repo.InstallModule(module.Name, url, settings.ModulesDir)
+
+			if err != nil {
+				msg := fmt.Sprintf("Failed to install module %q", module.Name)
+				if module.Version != "" {
+					msg += fmt.Sprintf(" in version %q", module.Version)
+				}
+				msg += "!"
+				fmt.Println(fmt.Errorf(msg))
+				continue
+			}
+			fmt.Printf("Module %q installed successfully.\n", module.Name)
+		}
 	},
 }
 
@@ -37,4 +70,29 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type Module struct {
+	Name    string
+	Version string
+}
+
+func getModules(args []string) []Module {
+	// Module names and versions are supplied on the command line like this:
+	// moduleA moduleB==v0.0.2 moduleC
+	// This function turns the slice of arguments into a slice of Module objects.
+
+	modules := make([]Module, 0)
+	for _, arg := range args {
+		if !strings.Contains(arg, "==") {
+			modules = append(modules, Module{arg, ""})
+		} else {
+			parts := strings.Split(arg, "==")
+			name := parts[0]
+			version := parts[1]
+			modules = append(modules, Module{name, version})
+		}
+	}
+
+	return modules
 }
