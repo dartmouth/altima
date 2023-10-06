92ceb617098e303b4c6845813d39ca1c0ef9df44
Simon Stone <simon.stone@dartmouth.edu>
2023-10-06 16:29:15 -0400

    adds function to delete a module's folder

--- pkg/repo/repo_test.go
+++ pkg/repo/repo_test.go
@@ -35,13 +35,38 @@ func TestSearch(t *testing.T) {
 
 func TestInstallModule(t *testing.T) {
 	settings := cli.New()
-	err := InstallModule(Module{Name: "cow", Alias: "mycow", Url: "https://github.com/crossett/altima-modules/releases/download/v0.0.1/cow-v0.0.1.tgz"}, settings.ModulesDir)
+	testModuleWithAlias := Module{Name: "cow", Alias: "mycow", Url: "https://github.com/crossett/altima-modules/releases/download/v0.0.1/cow-v0.0.1.tgz"}
+	err := InstallModule(testModuleWithAlias, settings.ModulesDir)
+	if err != nil {
+		t.Error("Could not install module!")
+	}
+	testModule := Module{Name: "cow", Url: "https://github.com/crossett/altima-modules/releases/download/v0.0.1/cow-v0.0.1.tgz"}
+	err = InstallModule(testModule, settings.ModulesDir)
+	if err != nil {
+		t.Error("Could not install module!")
+	}
+}
+
+func TestUninstallModule(t *testing.T){
+	settings := cli.New()
+	testModuleWithAlias := Module{Name: "cow", Alias: "mycow", Url: "https://github.com/crossett/altima-modules/releases/download/v0.0.1/cow-v0.0.1.tgz"}
+	err := InstallModule(testModuleWithAlias, settings.ModulesDir)
+	if err != nil {
+		t.Error("Could not install module!")
+	}
+	err = UninstallModule(testModuleWithAlias, settings.ModulesDir)
+	if err != nil {
+		t.Error("Could not uninstall module!")
+	}
+
+	testModule := Module{Name: "cow", Url: "https://github.com/crossett/altima-modules/releases/download/v0.0.1/cow-v0.0.1.tgz"}
+	err = InstallModule(testModule, settings.ModulesDir)
 	if err != nil {
 		t.Error("Could not install module!")
 	}
 
-	err = InstallModule(Module{Name: "cow", Url: "https://github.com/crossett/altima-modules/releases/download/v0.0.1/cow-v0.0.1.tgz"}, settings.ModulesDir)
+	err = UninstallModule(testModule, settings.ModulesDir)
 	if err != nil {
-		t.Error("Could not install module!")
+		t.Error("Could not uninstall module!")
 	}
 }
